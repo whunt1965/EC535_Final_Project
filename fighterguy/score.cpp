0f1167fe0ca10022ffd62bab7ae190b2b55fa248
Ken Krebs <56164075+ken-krebs@users.noreply.github.com>
2021-04-19 14:31:35 -0400

    Healthbar changes

---
+++ fighterguy/score.cpp
@@ -0,0 +1,31 @@
+#include "score.h"
+#include <QFont>
+
+
+//Score constructor
+Score::Score(QString pname, int score)
+    :score(score),
+     name(pname){
+
+    //Create Text Item
+    setPlainText(this->name + QString(" health:") + QString::number(this->score));
+    setDefaultTextColor(Qt::black);//we can play with this color
+    setFont(QFont("times",16));//and this font type and size
+
+}
+
+//Setter for score
+void Score::setScore(int score){
+    this->score = score;
+    setPlainText(this->name + QString(" health:") + QString::number(this->score));
+
+
+}
+
+//Getter for score
+int Score::getScore(){
+    return this->score;
+}
+
+
+
---
+++ fighterguy/score.h
@@ -0,0 +1,35 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+#include <QGraphicsTextItem>
+#include <QString>
+
+
+class Score: public QGraphicsTextItem{
+
+public:
+    /**
+     * @brief Score - Constructor for score
+     * @param pname - Player name tied to score
+     */
+    Score(QString pname, int score);
+
+    /**
+     * @brief setScore - Setter for score
+     * @param score - The new Score
+     */
+    void setScore(int score);
+
+    /**
+     * @brief getScore - Getter for Score
+     * @return score
+     */
+    int getScore();
+
+private:
+    int score;//player health
+    QString name;//player name
+
+};
+
+#endif // SCORE_H
